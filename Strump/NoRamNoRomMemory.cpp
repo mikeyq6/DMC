38975567d00f1b627837ac58c905a00431b70dc0
Michael Dann <michael.g.dann@gmail.com>
2021-02-23 08:46:22 +0000

    update basic memory type for GBC

--- Strump/NoRamNoRomMemory.cpp
+++ Strump/NoRamNoRomMemory.cpp
@@ -24,6 +24,16 @@ uint8_t NoRamNoRomMemory::internalReadMem(uint16_t location) {
 	else if (location >= 0x8000 && location < 0xc000) {
 		return internal_get(location);
 	}
+	// Undocumented registers
+	else if (location == FF6C) {
+		return rominfo->UseColour() ? internal_get(location) : 0xff;
+	}
+	else if (location == FF74) {
+		return rominfo->UseColour() ? internal_get(location) : 0xff;
+	}
+	else if (location == FF75) {
+		return internal_get(location) & 0x70;
+	}
 	else if (location == P1) { // Joypad register
 		uint8_t state = internal_get(P1);
 		if ((state & 0x10) == 0) // Bit 4 P14 low
@@ -41,19 +51,10 @@ uint8_t NoRamNoRomMemory::internalReadMem(uint16_t location) {
 void NoRamNoRomMemory::WriteMem(uint16_t location, uint8_t value) {
 	std::lock_guard<mutex> locker(mem_mutex);
 
-	if (value == 0xca) {
-		int x = 1;
-	}
-	if (location == IE) {
-		int x = 1;
-	}
 	if (location == DMA) {
 		doDMATransfer(value);
 	}
 	else if (location == P1) {
-		if (value == 0x20) {
-			int g = 1;
-		}
 		internal_set(location, value);
 	}
 	else if (location == DIV) {
@@ -62,34 +63,71 @@ void NoRamNoRomMemory::WriteMem(uint16_t location, uint8_t value) {
 	else if (location == ENDSTART) {
 		Startup = false;
 	}
-	//else if (rominfo->CartInfo->controllerType == NO_ROMBANK) {
-	//	if (location >= 0x8000) {
-	//		internal_set(location, value);
-	//	}
-	//} 
+	else if(location == BCPS && rominfo->UseColour()) {
+		// printf("BCPS: %x\n", value);
+		setPaletteWrite(false, value);
+	}
+	else if(location == BCPD && rominfo->UseColour()) {
+		// printf("BCPD: %x\n", value);
+		setPaletteData(value);
+	}
+	else if(location == OCPS && rominfo->UseColour()) {
+		// printf("OCPS: %x\n", value);
+		setPaletteWrite(true, value);
+	}
+	else if(location == OCPD && rominfo->UseColour()) {
+		// printf("OCPD: %x\n", value);
+		setPaletteData(value);
+	}
+	else if(location == HDMA1) {
+		printf("HDMA1 value: %x\n", value);
+		setHDMASourceHigh(value);
+	}
+	else if(location == HDMA2) {
+		printf("HDMA2 value: %x\n", value);
+		setHDMASourceLow(value);
+	}
+	else if(location == HDMA3) {
+		printf("HDMA3 value: %x\n", value);
+		setHDMADestinationHigh(value);
+	}
+	else if(location == HDMA4) {
+		printf("HDMA4 value: %x\n", value);
+		setHDMADestinationLow(value);
+	}
+	else if(location == HDMA5) {
+		printf("HDMA5 value: %x\n", value);
+		printf("source: %x, dest: %x\n", dmaSource, dmaDestination);
+		doHDMATransfer(value);
+	}
+	// Undocumented registers
+	else if (location == FF6C) {
+		if(rominfo->UseColour())
+			internal_set(location, 0xfe + (value & 1));
+	}
+	else if (location == FF74) {
+		if(rominfo->UseColour())
+			internal_set(location, value);
+	}
+	else if (location == FF75) {
+		internal_set(location, value & 0x70);
+	}
 	else if (location >= 0 && location < 0x8000) {
 		printf("No Ram/Rom Banking allowed at address(%04x)\n", value);
 	}
-	else if (location >= 0xa000 && location < 0xc000) { // Writing to RAM
-		if (RamEnabled) {
-			cout << "Trying to write to RAM but it is not enabled" << endl;
-		}
+	else if (location >= 0xc000 && location < 0xe000) {
+		if (location + 0x2000 < 0xfe00)
+			internal_set(location + 0x2000, value);
+		internal_set(location, value);
 	}
 	else if (location >= 0xe000 && location < 0xfe00) { // Allow for the mirrored internal RAM
 		internal_set(location - 0x2000, value);
-		internal_set(location, value);
-	}
-	else if (location >= 0xc000 && location < 0xe000) { // Allow for the mirrored internal RAM
-		if (location + 0x2000 < 0xfe00)
-			internal_set(location + 0x2000, value);
 		internal_set(location, value);
 	}
 	else if (location == LY) {
 		internal_set(LY, 0);
 	}
 	else if (location >= 0x9000 && location <= 0x98ff) {
-		if (value == 0x30)
-			int c = 1;
 		internal_set(location, value);
 	}
 	else {
